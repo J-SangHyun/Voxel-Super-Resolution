bc4032da7cc7b84b3b5edf71445e850571581921
정상현 <96582264+J-SangHyun@users.noreply.github.com>
2022-08-17 01:13:00 +0900

    dataloader upsampling added

--- dataloader.py
+++ dataloader.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 import torch
 from pathlib import Path
+import scipy.ndimage as nd
 from torch.utils.data import Dataset
 from utils.voxel_functions import binvox2numpy
 
 
 class ModelNetDataset(Dataset):
-    def __init__(self, dataset, mode, lr, hr):
+    def __init__(self, dataset, mode, lr, hr, upsampling=False):
         super(ModelNetDataset, self).__init__()
         self.lr, self.hr = lr, hr
         self.rate = hr // lr
         self.dataset = dataset
         self.mode = mode
+        self.upsampling = upsampling
 
         assert dataset in ['ModelNet10', 'ModelNet40']
         assert mode in ['train', 'val', 'test']
+        assert hr > lr and hr % lr == 0
 
         self.lr_files, self.hr_files = [], []
         lr_classes = Path(f'voxelized/{lr}/{dataset}/').glob('*')
@@ -42,9 +45,11 @@ class ModelNetDataset(Dataset):
         assert len(self.lr_files) == len(self.hr_files)
 
     def __getitem__(self, idx):
-        lr = torch.Tensor(binvox2numpy(self.lr_files[idx]))
-        hr = torch.Tensor(binvox2numpy(self.hr_files[idx]))
-        return lr, hr
+        lr = binvox2numpy(self.lr_files[idx])
+        if self.upsampling:
+            lr = nd.zoom(lr, (self.rate, self.rate, self.rate), mode='constant', order=0)
+        hr = binvox2numpy(self.hr_files[idx])
+        return torch.Tensor(lr), torch.Tensor(hr)
 
     def __len__(self):
         return len(self.lr_files)
